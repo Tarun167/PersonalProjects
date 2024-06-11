d1e447f74f0eefba218a7c52f49047e03658775f
Tarun167 <tarunalpha25@gmail.com>
2024-06-11 20:41:09 +0530

    fix:class method name changes

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import kivy
 from kivy.uix.label import Label
 from kivy.app import App
 
-
 class MyApp(App):
-    def bulid(self):
+    def build(self):
         return Label(text="Hello")
 
 
 if __name__ == "__main__":
     MyApp().run()
-    
